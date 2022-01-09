d8beb8cf99678d51952c994a7d53eaeff9cb5b8e
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-01-09 22:14:04 +1100

    Refactored previous code, implemented menu loop until allowed input is entered

--- MCBAInternetBanking/ConsoleApp1/Program.cs
+++ MCBAInternetBanking/ConsoleApp1/Program.cs
@@ -2,43 +2,100 @@
 
 using System.Text;
 
-Console.Write("Enter Login ID: ");
-string? loginIdInput = Console.ReadLine();
-
-Console.Write("Enter Password: ");
-bool passwordInputComplete = false;
-StringBuilder passwordBuilder = new StringBuilder();
-// Hiding the password and instead displaying * for each read character other than enter
-while (!passwordInputComplete)
+namespace MCBAConsole;
+public class Program
 {
-    ConsoleKeyInfo inputKey = Console.ReadKey(true);
-    if (inputKey.Key != ConsoleKey.Enter)
+    private static void Main()
     {
-        passwordBuilder.Append(inputKey.KeyChar);
-        Console.Write("*");
+        var (loginId, password) = Login();
+        string menu = CreateMenuString(loginId);
+
+        MenuLoop(menu);
     }
-    else { passwordInputComplete = true; }
-}
 
-StringBuilder menuOptionsStringBuilder = new StringBuilder();
-string[] menuOptions = new[]
+    private static int MenuLoop(string menu)
     {
-        "",
-        "",
-        $"--- {loginIdInput} ---",
-        "[1] Deposit",
-        "[2] Withdraw",
-        "[3] Transfer",
-        "[4] Logout",
-        "[5] Exit",
-        "",
-        "Enter an option: "
-    };
-foreach (string MenuOption in menuOptions)
-{
-    menuOptionsStringBuilder.AppendLine(MenuOption);
+        bool menuRunning = true;
+        int selectedOption = 0;
+        while (menuRunning)
+        {
+            int? menuInput = MenuInput(menu);
+
+            if (menuInput.HasValue && (menuInput > 0 && menuInput < 6))
+            {
+                menuRunning = false;
+                selectedOption = menuInput.Value;
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a Number From 1-5");
+            }
+        }
+
+        return selectedOption;
+    }
+
+    private static int? MenuInput(string menu)
+    {
+        Console.Write(menu);
+        ConsoleKeyInfo inputKey = Console.ReadKey();
+        // Ensuring menu has new line after input
+        Console.WriteLine("");
+        try
+        {
+            return int.Parse(inputKey.KeyChar.ToString());
+        } catch (FormatException)
+        {
+            return null;
+        }
+    }
+
+    // Displays the login prompt and returns the LoginId and Password entered 
+    private static (string? loginId, string password) Login()
+    {
+        Console.Write("Enter Login ID: ");
+        string? loginIdInput = Console.ReadLine();
+
+        Console.Write("Enter Password: ");
+        bool passwordInputComplete = false;
+        StringBuilder passwordBuilder = new StringBuilder();
+        // Hiding the password and instead displaying * for each read character other than enter
+        while (!passwordInputComplete)
+        {
+            ConsoleKeyInfo inputKey = Console.ReadKey(true);
+            if (inputKey.Key != ConsoleKey.Enter)
+            {
+                passwordBuilder.Append(inputKey.KeyChar);
+                Console.Write("*");
+            }
+            else { passwordInputComplete = true; }
+        }
+       
+        return (loginId: loginIdInput, password: passwordBuilder.ToString());
+    }
+
+    private static string CreateMenuString(string? user)
+    {
+        StringBuilder menuOptionsStringBuilder = new StringBuilder();
+        string[] menuOptions = new[]
+            {
+                "",
+                "",
+                $"--- {user} ---",
+                "[1] Deposit",
+                "[2] Withdraw",
+                "[3] Transfer",
+                "[4] Logout",
+                "[5] Exit",
+                "",
+                "Enter an option: "
+            };
+        foreach (string MenuOption in menuOptions)
+        {
+            menuOptionsStringBuilder.AppendLine(MenuOption);
+        }
+
+        // Using TrimEnd() to remove the new line added in above foreach loop to allow input to be on same line as text
+        return menuOptionsStringBuilder.ToString().TrimEnd();
+    }
 }
-
-// Using TrimEnd() to remove the new line added in above foreach loop to allow input to be on same line as text
-Console.Write(menuOptionsStringBuilder.ToString().TrimEnd());
-
