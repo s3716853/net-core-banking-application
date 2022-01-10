732b66f8895c44f4a50eab780e56b4988a77a556
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-01-10 14:11:42 +1100

    Further refactoring of console classes
    Created IConsoleMenu interface which defines the different menus of the application

---
+++ MCBAInternetBanking/ConsoleApp1/Menu/LoginMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBAConsole.Menu;
+
+internal class LoginMenu : IConsoleMenu
+{
+    public LoginMenu(){}
+
+    public void Run()
+    {
+        bool loggedIn = false;
+
+        while (!loggedIn)
+        {
+            var (loginId, password) = DisplayMenu();
+
+            UserContext userContext = UserContext.GetInstance();
+
+            if (UserContext.Verify(loginId, password))
+            {
+                // A verified password will never have a null loginid 
+                #pragma warning disable CS8601 // Possible null reference assignment.
+                userContext.Username = loginId;
+                userContext.Password = password;
+                loggedIn = true;
+            }
+        }
+
+    }
+
+    private static (string? loginId, string password) DisplayMenu()
+    {
+        Console.Write("Enter Login ID: ");
+        string? loginIdInput = Console.ReadLine();
+
+        Console.Write("Enter Password: ");
+        bool passwordInputComplete = false;
+        StringBuilder passwordBuilder = new StringBuilder();
+        // Hiding the password and instead displaying * for each read character other than enter
+        while (!passwordInputComplete)
+        {
+            ConsoleKeyInfo inputKey = Console.ReadKey(true);
+            if (inputKey.Key != ConsoleKey.Enter)
+            {
+                passwordBuilder.Append(inputKey.KeyChar);
+                Console.Write("*");
+            }
+            else { passwordInputComplete = true; }
+        }
+        // Ensurinng there is a new line after login
+        Console.WriteLine();
+        return (loginId: loginIdInput, password: passwordBuilder.ToString());
+    }
+}
+
--- MCBAInternetBanking/ConsoleApp1/Program.cs
+++ MCBAInternetBanking/ConsoleApp1/Program.cs
@@ -1,101 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Text;
+using MCBAConsole.Menu;
 
 namespace MCBAConsole;
 public class Program
 {
     private static void Main()
     {
-        var (loginId, password) = Login();
-        string menu = CreateMenuString(loginId);
-
-        MenuLoop(menu);
-    }
-
-    private static int MenuLoop(string menu)
-    {
-        bool menuRunning = true;
-        int selectedOption = 0;
-        while (menuRunning)
-        {
-            int? menuInput = MenuInput(menu);
-
-            if (menuInput.HasValue && (menuInput > 0 && menuInput < 6))
-            {
-                menuRunning = false;
-                selectedOption = menuInput.Value;
-            }
-            else
-            {
-                Console.WriteLine("Please Enter a Number From 1-5");
-            }
-        }
-
-        return selectedOption;
-    }
-
-    private static int? MenuInput(string menu)
-    {
-        Console.Write(menu);
-        ConsoleKeyInfo inputKey = Console.ReadKey();
-        // Ensuring menu has new line after input
-        Console.WriteLine("");
-        try
-        {
-            return int.Parse(inputKey.KeyChar.ToString());
-        } catch (FormatException)
-        {
-            return null;
-        }
-    }
-
-    // Displays the login prompt and returns the LoginId and Password entered 
-    private static (string? loginId, string password) Login()
-    {
-        Console.Write("Enter Login ID: ");
-        string? loginIdInput = Console.ReadLine();
-
-        Console.Write("Enter Password: ");
-        bool passwordInputComplete = false;
-        StringBuilder passwordBuilder = new StringBuilder();
-        // Hiding the password and instead displaying * for each read character other than enter
-        while (!passwordInputComplete)
-        {
-            ConsoleKeyInfo inputKey = Console.ReadKey(true);
-            if (inputKey.Key != ConsoleKey.Enter)
-            {
-                passwordBuilder.Append(inputKey.KeyChar);
-                Console.Write("*");
-            }
-            else { passwordInputComplete = true; }
-        }
-       
-        return (loginId: loginIdInput, password: passwordBuilder.ToString());
-    }
-
-    private static string CreateMenuString(string? user)
-    {
-        StringBuilder menuOptionsStringBuilder = new StringBuilder();
-        string[] menuOptions = new[]
-            {
-                "",
-                "",
-                $"--- {user} ---",
-                "[1] Deposit",
-                "[2] Withdraw",
-                "[3] Transfer",
-                "[4] Logout",
-                "[5] Exit",
-                "",
-                "Enter an option: "
-            };
-        foreach (string MenuOption in menuOptions)
-        {
-            menuOptionsStringBuilder.AppendLine(MenuOption);
-        }
-
-        // Using TrimEnd() to remove the new line added in above foreach loop to allow input to be on same line as text
-        return menuOptionsStringBuilder.ToString().TrimEnd();
+        new MainMenu().Run();
     }
 }
