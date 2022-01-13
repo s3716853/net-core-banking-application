2d9b9989a92b747da44ee190011c42b6998b5404
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-01-13 17:23:51 +1100

    Created Extension methods to assist in printing model types

---
+++ MCBAInternetBanking/RequestManagement/Utilities/AccountExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RequestManagement.Model;
+
+namespace RequestManagement.Utilities
+{
+    public static class AccountExtension
+    {
+        // Cannot override ToString with extension methods
+        // Chose to use an Extension method instead of implementing an overrided ToString
+        // in the model classes as to keep them purely as data classes
+        public static string AsString(this Account account)
+        {
+            string transactionStrings = "";
+            account.Transactions.ForEach(transaction =>
+            {
+                transactionStrings += transaction.AsString();
+            });
+            return new StringBuilder().AppendArray(new string[]
+            {
+                $"AccountNumber={account.AccountNumber}",
+                $"AccountType={account.AccountType}",
+                $"Balance={account.Balance}",
+                $"CustomerID={account.CustomerID}",
+                "-Transactions-",
+                transactionStrings
+            }).ToString();
+        }
+    }
+}
---
+++ MCBAInternetBanking/RequestManagement/Utilities/LoginExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RequestManagement.Model;
+
+namespace RequestManagement.Utilities
+{
+    public static class LoginExtension
+    {
+        // Cannot override ToString with extension methods
+        // Chose to use an Extension method instead of implementing an overrided ToString
+        // in the model classes as to keep them purely as data classes
+        public static string AsString(this Login login)
+        {
+            return new StringBuilder().AppendArray(new string[]
+            {
+                $"CustomerID={login.CustomerID}",
+                $"LoginID={login.LoginID}",
+                $"PasswordHash={login.PasswordHash}"
+            }).ToString();
+        }
+    }
+}
