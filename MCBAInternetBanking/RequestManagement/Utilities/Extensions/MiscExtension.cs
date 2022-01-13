6864f2b8b10a59cb3d77f333c3d300493b1a98b8
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-01-13 21:10:14 +1100

    Create MiscExtension.cs

---
+++ MCBAInternetBanking/RequestManagement/Utilities/Extensions/MiscExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace RequestManagement.Utilities.Extensions
+{
+    public static class MiscExtension
+    {
+        // Taken from the lectures (Week 3), thanks for the method!
+        public static DataTable GetDataTable(this SqlCommand command)
+        {
+            using var adapter = new SqlDataAdapter(command);
+
+            var table = new DataTable();
+            adapter.Fill(table);
+
+            return table;
+        }
+    }
+}
