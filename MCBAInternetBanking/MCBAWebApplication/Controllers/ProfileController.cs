a45e054bd71c6d3f33f3f48481a93fb494749c0d
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-01-29 20:57:11 +1100

    Added some client side password validation

--- MCBAInternetBanking/MCBAWebApplication/Controllers/ProfileController.cs
+++ MCBAInternetBanking/MCBAWebApplication/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using MCBABackend.Models;
 using MCBAWebApplication.Models.ViewModels;
+using MCBAWebApplication.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MCBAWebApplication.Controllers;
@@ -9,9 +10,53 @@ public class ProfileController : McbaController
     public ProfileController(ILogger<DepositController> logger, IServiceProvider serviceProvider, IConfiguration configuration) : base(logger, serviceProvider, configuration)
     {
     }
-    public async Task<IActionResult> Index(ProfileViewModel? profileViewModel)
+    public async Task<IActionResult> Index()
     {
         _logger.LogInformation("GET: Profile/");
+
+        return View(await GetDefaultViewModel());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Customer([FromForm] ProfileCustomerViewModel profileCustomerViewModel)
+    {
+        _logger.LogInformation("POST: Profile/Customer");
+
+        if (!ModelState.IsValid)
+        {
+            ProfileViewModel profileViewModel = await GetDefaultViewModel();
+            profileViewModel.ProfileCustomerViewModel = profileCustomerViewModel;
+            return View("Index", profileViewModel);
+        }
+
+        await PostQueryCustomerApi($"{_connectionString}/Customer", profileCustomerViewModel);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login([FromForm] ProfileLoginViewModel profileLoginViewModelViewModel)
+    {
+        _logger.LogInformation("POST: Profile/Login");
+
+        ProfileActionValidation.Login(ModelState, profileLoginViewModelViewModel.PasswordOldHash, profileLoginViewModelViewModel.PasswordOld);
+
+        if (!ModelState.IsValid)
+        {
+            ProfileViewModel profileViewModel = await GetDefaultViewModel();
+            profileViewModel.ProfileLoginViewModel = profileLoginViewModelViewModel;
+            return View("Index", profileViewModel);
+        }
+
+        await PostQueryCustomerApi($"{_connectionString}/Login", profileLoginViewModelViewModel);
+
+        return RedirectToAction("Index");
+    }
+
+    private async Task<ProfileViewModel> GetDefaultViewModel()
+    {
         Customer customer = await GetLoggedInCustomer();
 
         ProfileCustomerViewModel profileCustomerViewModel = new ProfileCustomerViewModel()
@@ -34,55 +79,11 @@ public class ProfileController : McbaController
             PasswordOldHash = customer.Login.PasswordHash
         };
 
-        if (profileViewModel != null)
+        return new ProfileViewModel()
         {
-            profileViewModel.ProfileCustomerViewModel ??= profileCustomerViewModel;
-            profileViewModel.ProfileLoginViewModel ??= profileLoginViewModel;
-
-            return View(profileViewModel);
-        }
-
-        return View(new ProfileViewModel()
-        {
-            ProfileLoginViewModel = profileLoginViewModel,
-            ProfileCustomerViewModel = profileCustomerViewModel
-        });
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Customer([FromForm] ProfileCustomerViewModel profileCustomerViewModel)
-    {
-        _logger.LogInformation("POST: Profile/Customer");
-
-        if (!ModelState.IsValid)
-            return View("Index", new ProfileViewModel()
-            {
-                ProfileLoginViewModel = null,
-                ProfileCustomerViewModel = profileCustomerViewModel
-            });
-
-        await PostQueryCustomerApi($"{_connectionString}/Customer", profileCustomerViewModel);
-
-        return RedirectToAction("Index");
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Login([FromForm] ProfileLoginViewModel profileLoginViewModelViewModel)
-    {
-        _logger.LogInformation("POST: Profile/Login");
-
-        if (!ModelState.IsValid)
-            return View("Index", new ProfileViewModel()
-            {
-                ProfileLoginViewModel = profileLoginViewModelViewModel,
-                ProfileCustomerViewModel = null
-            });
-
-        await PostQueryCustomerApi($"{_connectionString}/Login", profileLoginViewModelViewModel);
-
-        return RedirectToAction("Index");
+            ProfileCustomerViewModel = profileCustomerViewModel,
+            ProfileLoginViewModel = profileLoginViewModel
+        };
     }
 
 }
---
+++ MCBAInternetBanking/MCBAWebApplication/Utilities/ProfileActionValidation.cs
@@ -0,0 +1,20 @@
+﻿using MCBABackend.Models;
+using MCBABackend.Utilities;
+using MCBABackend.Utilities.Extensions;
+using MCBAWebApplication.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SimpleHashing;
+
+namespace MCBAWebApplication.Utilities
+{
+    internal static class ProfileActionValidation
+    {
+        public static void Login(ModelStateDictionary modelState, string passwordHash, string password)
+        {
+            if (!PBKDF2.Verify(passwordHash, password))
+            {
+                modelState.AddModelError("", "Incorrect password");
+            }
+        }
+    }
+}
